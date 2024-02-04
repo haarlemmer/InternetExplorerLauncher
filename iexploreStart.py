f27bf7c6253ad88e6a6fcd52c3f72bf1ea8eaeef
haarlemmer <hihaar@outlook.com>
2024-02-04 16:09:02 +0800

    Add GPL head to iexploreLaunch.py

--- iexploreStart.py
+++ iexploreStart.py
@@ -1,3 +1,21 @@
+#    Internet Explorer Launcher for Windows 10 22H2 or larer version that can't launch it directly.
+#    Copyright (C) 2024 Jiangbo Liu
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with this program; if not, write to the Free Software Foundation, Inc.,
+#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+
 import sys, re, os
 
 def getIexploreStartCmd(iexplorePath, url):
